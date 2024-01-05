1978df6b246771960b9993cbf0d5f8c6b0c63272
e277 <ezramuir12@gmail.com>
2024-01-05 10:41:09 -0500

    File Uploading Tutorial

---
+++ file-uploading/src/main/java/ungu/fileuploading/storage/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package ungu.fileuploading.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
