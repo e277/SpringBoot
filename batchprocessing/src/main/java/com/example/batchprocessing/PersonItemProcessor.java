c76a9d928853d300df0073cc9c30b8b8f0a0382f
e277 <ezramuir12@gmail.com>
2024-01-05 13:46:10 -0500

    Fixed null warning

--- batchprocessing/src/main/java/com/example/batchprocessing/PersonItemProcessor.java
+++ batchprocessing/src/main/java/com/example/batchprocessing/PersonItemProcessor.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
 
 public class PersonItemProcessor implements ItemProcessor<Person, Person> {
     private static final Logger log = LoggerFactory.getLogger(PersonItemProcessor.class);
 
     @Override
-    public Person process(final Person person) {
+    public Person process(@NonNull final Person person) {
         final String firstName = person.firstName().toUpperCase();
         final String lastName = person.lastName().toUpperCase();
 
@@ -20,4 +21,5 @@ public class PersonItemProcessor implements ItemProcessor<Person, Person> {
         return transformedPerson;
     }
 }
+
 
